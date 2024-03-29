a9b64c0cd45eaac8eb42168bfc0167eae64e1324
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-03-29 18:23:54 +0100

    bring back nvim

---
+++ .config/nvim/lua/hyrik/remap.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+
+-- go to netwrk
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+
