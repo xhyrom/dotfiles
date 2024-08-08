fb76f298577c809358bc3442f77ee186cc65789a
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-08-08 12:42:31 +0200

    fix: dump handle dirnames, existing dirs

--- common/dump.py
+++ common/dump.py
@@ -8,9 +8,16 @@ def dump(stuff: list[str]):
 
         for path in glob(expand, recursive=True):
             normalized_path = path.removeprefix(os.path.expanduser("~/"))
-            if os.path.isdir(normalized_path) and not os.path.exists(normalized_path):
-                os.mkdir(normalized_path)
-            elif os.path.isfile(normalized_path):
+            if os.path.isdir(path):
+                if os.path.exists(normalized_path):
+                    shutil.rmtree(normalized_path)
+
+                os.makedirs(normalized_path)
+            else:
+                dirname = os.path.dirname(normalized_path)
+                if dirname:
+                    os.makedirs(dirname, exist_ok=True)
+
                 shutil.copy2(path, normalized_path)
 
     exit(1)
