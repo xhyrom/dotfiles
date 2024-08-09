00787908461f6e37bc37a1208f39c1010bd7a72c
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-08-09 15:37:10 +0200

    fish: add things to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,8 @@
 set -g fish_greeting
+
+alias ls="lsd"
+alias cat="bat"
+alias s="kitten ssh"
+
+
+fish_add_path -g ~/.bun/bin ~/.local/bin ~/go/bin ~/.wakatime ~/.rbenv/shims ~/.moon/bin ~/.cargo/bin
