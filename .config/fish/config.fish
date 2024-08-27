ed3d3227841f858d92d4dabe6fd8b67d30c49c3a
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-08-27 15:43:14 +0200

    fish: add rbenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,6 @@ alias s="kitten ssh"
 
 
 fish_add_path -g ~/.bun/bin ~/.local/bin ~/go/bin ~/.wakatime ~/.rbenv/shims ~/.moon/bin ~/.cargo/bin ~/.fvm_flutter/bin
+
+# Added by `rbenv init` on Tue 27 Aug 15:42:29 CEST 2024
+status --is-interactive; and rbenv init - --no-rehash fish | source
