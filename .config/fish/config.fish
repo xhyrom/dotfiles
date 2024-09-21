07b6cedee139fbaf1b1afc2a9ea8aea377d3f417
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-09-21 18:14:02 +0200

    fish: add inshellisense

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,12 @@ fish_add_path -g ~/.bun/bin ~/.local/bin ~/go/bin ~/.wakatime ~/.rbenv/shims ~/.
 
 # Added by `rbenv init` on Tue 27 Aug 15:42:29 CEST 2024
 status --is-interactive; and rbenv init - --no-rehash fish | source
+
+# ---------------- inshellisense shell plugin ----------------
+if test -z "$ISTERM" && status --is-interactive
+  if status --is-login
+    is -s fish --login ; kill %self
+  else
+    is -s fish ; kill %self
+  end 
+end
