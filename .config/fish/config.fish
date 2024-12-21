47161e4d8466beaecd44165410b3dd2cbcf1ad3e
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-12-21 11:56:42 +0100

    zed, vesktop, other

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,16 +4,16 @@ alias ls="lsd"
 alias cat="bat"
 alias s="kitten ssh"
 
-fish_add_path -g ~/.bun/bin ~/.local/bin ~/go/bin ~/.wakatime ~/.rbenv/shims ~/.moon/bin ~/.cargo/bin ~/.fvm_flutter/bin
+fish_add_path -g ~/.bun/bin ~/.local/bin ~/go/bin ~/.wakatime ~/.rbenv/shims ~/.moon/bin ~/.cargo/bin ~/.fvm_flutter/bin ~/.crystal/bin ~/fvm/default/bin
 
 # Added by `rbenv init` on Tue 27 Aug 15:42:29 CEST 2024
 status --is-interactive; and rbenv init - --no-rehash fish | source
 
 # ---------------- inshellisense shell plugin ----------------
-if test -z "$ISTERM" && status --is-interactive
-  if status --is-login
-    is -s fish --login ; kill %self
-  else
-    is -s fish ; kill %self
-  end 
-end
+#if test -z "$ISTERM" && status --is-interactive
+#  if status --is-login
+#    is -s fish --login ; kill %self
+#  else
+#    is -s fish ; kill %self
+#  end 
+#end
--- .config/nvim/lua/hyrik/set.lua
+++ .config/nvim/lua/hyrik/set.lua
@@ -12,7 +12,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/vim/undodir"
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
